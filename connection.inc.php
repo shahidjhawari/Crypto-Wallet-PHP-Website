0ae4094442ad10f812d5e6b8b02d8002f99fbc98
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-05-18 08:10:31 -0700

    latest

---
+++ connection.inc.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+$con=mysqli_connect("localhost","root","","coin");
+define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/coin');
+define('SITE_PATH','http://127.0.0.1/coin');
+
+define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
+define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
+?>
