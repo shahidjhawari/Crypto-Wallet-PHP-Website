83e46c91ac832f7dff1f23e576260acc4967014f
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-05-18 17:03:02 -0700

    new

--- login_submit.php
+++ login_submit.php
@@ -1,9 +1,8 @@
 <?php
 require('connection.inc.php');
-require('functions.inc.php');
 
-$email=get_safe_value($con,$_POST['email']);
-$password=get_safe_value($con,$_POST['password']);
+// $email=get_safe_value($con,$_POST['email']);
+// $password=get_safe_value($con,$_POST['password']);
 
 $res=mysqli_query($con,"select * from users where email='$email' and password='$password'");
 $check_user=mysqli_num_rows($res);
@@ -13,10 +12,6 @@ if($check_user>0){
 	$_SESSION['USER_ID']=$row['id'];
 	$_SESSION['USER_NAME']=$row['name'];
 	
-	if(isset($_SESSION['WISHLIST_ID']) && $_SESSION['WISHLIST_ID']!=''){
-		wishlist_add($con,$_SESSION['USER_ID'],$_SESSION['WISHLIST_ID']);
-		unset($_SESSION['WISHLIST_ID']);
-	}
 	
 	echo "valid";
 }else{
