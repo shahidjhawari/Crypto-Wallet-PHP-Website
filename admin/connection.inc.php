f97ce10d9a128e16ef51e63a147ed1a19131ca58
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-05-21 20:24:31 +0500

    admin sucessful implement

--- admin/connection.inc.php
+++ admin/connection.inc.php
@@ -1,9 +1,9 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "coin");
-define('SERVER_PATH', $_SERVER['DOCUMENT_ROOT'] . '/coin');
-define('SITE_PATH', 'http://localhost/coin');
+session_start();
+$con=mysqli_connect("localhost","root","","coin");
+define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/coin');
+define('SITE_PATH','http://localhost/coin/');
 
-define('PRODUCT_IMAGE_SERVER_PATH', SERVER_PATH . 'media/product/');
-define('PRODUCT_IMAGE_SITE_PATH', SITE_PATH . 'media/product/');
-
-define('UPLOAD_DIR', 'C:/xampp/htdocs/coin/media/product/');
+define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
+define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
+?>
