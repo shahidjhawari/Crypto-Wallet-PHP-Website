b99714b57f7c0c6f5ad16ae80d1583f15de18054
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-05-29 20:08:33 +0500

    latest

--- admin/admin.php
+++ admin/admin.php
@@ -11,7 +11,7 @@ require('top.inc.php');
 <div class="container">
     <h2>Admin Panel: Manage Daily Earnings</h2>
     <form method="post" action="calculate_daily_earnings.php">
-        <button type="submit" name="percentage" value="0.0045" class="btn btn-primary">Earn 0.45%</button>
+        <button type="submit" name="percentage" value="0.0035" class="btn btn-primary">Earn 0.45%</button>
         <button type="submit" name="percentage" value="0.0055" class="btn btn-primary">Earn 0.55%</button>
         <button type="submit" name="percentage" value="0.0065" class="btn btn-primary">Earn 0.65%</button>
     </form>
