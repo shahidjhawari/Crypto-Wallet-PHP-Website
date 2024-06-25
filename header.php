8ce1d8370863b4f4267af3c656733f0bbda0d41d
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-06-25 15:05:25 +0500

    latest

--- header.php
+++ header.php
@@ -38,6 +38,9 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
+                        <a class="nav-link" href="#">Wallet</a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" href="#">Stacking</a>
                     </li>
                     <li class="nav-item">
