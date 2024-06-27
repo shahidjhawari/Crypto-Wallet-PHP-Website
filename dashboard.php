597eaa6279b179cb1e620486e10e0fdc3ab760b1
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-06-27 06:02:20 +0500

    latest

--- dashboard.php
+++ dashboard.php
@@ -291,47 +291,47 @@ foreach ($staking_records as $record) {
 
 
         <div class="col-12 mb-4">
-    <div class="card">
-        <div class="card-body p-3">
-            <div class="row">
-                <div class="col-12">
-                    <p class="fs-5 mb-3">On Stacking</p>
-                    <?php
-                    // Check if there are any staking records and get the latest one, otherwise set to 0
-                    $latest_staking_record = !empty($staking_records) ? end($staking_records) : ['amount' => 0];
-                    ?>
-                    <h1 class="display-5 mb-4" style="margin-top: -15px;">
-                        <?php echo '$' . htmlspecialchars(number_format($latest_staking_record['amount'], 2)); ?>
-                    </h1>
-                    <!-- Remove the loop as we are only showing the latest record -->
-                    <!-- <p><a href="#" class="btn btn-info">Stacking</a></p> -->
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="fs-5 mb-3">On Stacking</p>
+                            <?php
+                            // Check if there are any staking records and get the latest one, otherwise set to 0
+                            $latest_staking_record = !empty($staking_records) ? end($staking_records) : ['amount' => 0];
+                            ?>
+                            <h1 class="display-5 mb-4" style="margin-top: -15px;">
+                                <?php echo '$' . htmlspecialchars(number_format($latest_staking_record['amount'], 2)); ?>
+                            </h1>
+                            <!-- Remove the loop as we are only showing the latest record -->
+                            <!-- <p><a href="#" class="btn btn-info">Stacking</a></p> -->
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
 
-<div class="col-12 mb-4">
-    <div class="card">
-        <div class="card-body p-3">
-            <div class="row">
-                <div class="col-12">
-                    <p class="fs-5 mb-3">Daily Earning</p>
-                    <?php
-                    // Check if there are any daily earnings records and get the latest one, otherwise set to 0
-                    $latest_daily_earning_record = !empty($daily_earnings_records) ? end($daily_earnings_records) : ['amount' => 0, 'date' => ''];
-                    ?>
-                    <h1 class="display-5 mb-4" style="margin-top: -15px;">
-                        <?php echo '$' . htmlspecialchars(number_format($latest_daily_earning_record['amount'], 2)); ?>
-                    </h1>
-                    <p><?php echo htmlspecialchars($latest_daily_earning_record['date']); ?></p>
-                    <!-- Remove the loop as we are only showing the latest record -->
-                    <!-- <p><a href="#" class="btn btn-info">Daily Earning Record</a></p> -->
+        <div class="col-12 mb-4">
+            <div class="card">
+                <div class="card-body p-3">
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="fs-5 mb-3">Daily Earning</p>
+                            <?php
+                            // Check if there are any daily earnings records and get the latest one, otherwise set to 0
+                            $latest_daily_earning_record = !empty($daily_earnings_records) ? end($daily_earnings_records) : ['amount' => 0, 'date' => ''];
+                            ?>
+                            <h1 class="display-5 mb-4" style="margin-top: -15px;">
+                                <?php echo '$' . htmlspecialchars(number_format($latest_daily_earning_record['amount'], 2)); ?>
+                            </h1>
+                            <p><?php echo htmlspecialchars($latest_daily_earning_record['date']); ?></p>
+                            <!-- Remove the loop as we are only showing the latest record -->
+                            <!-- <p><a href="#" class="btn btn-info">Daily Earning Record</a></p> -->
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
 
 
 
