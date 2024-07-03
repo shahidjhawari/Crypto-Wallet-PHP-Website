277144bdf0d23f26900f4dac2b4ec9acf07d433f
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-07-03 13:00:27 +0500

    latest

--- dashboard.php
+++ dashboard.php
@@ -291,6 +291,8 @@ $stmt->execute();
 $transaction_status_row = $stmt->get_result()->fetch_assoc();
 $transaction_status = $transaction_status_row['status'] ?? null;
 $stmt->close();
+
+$record = isset($record) ? $record : ['total_earning' => 0];
 ?>
 
 
@@ -416,19 +418,28 @@ $stmt->close();
         </div>
 
         <div class="col-12 mb-4">
-            <div class="card">
-                <div class="card-body p-3">
-                    <div class="row">
-                        <div class="col-12">
-                            <p class="fs-5 mb-3">Total Earning</p>
-                            <h1 class="display-5 mb-4" style="margin-top: -15px;">$<?php echo $user_rewards['reward_points'] + htmlspecialchars(number_format($record['total_earning'], 2)); ?></h1>
-                            <!-- <p><a href="team.php" class="btn btn-info">Team Building</a></p> -->
-                             <p>Daily & Refer Total Earning</p>
-                        </div>
-                    </div>
+    <div class="card">
+        <div class="card-body p-3">
+            <div class="row">
+                <div class="col-12">
+                    <p class="fs-5 mb-3">Total Earning</p>
+                    <h1 class="display-5 mb-4" style="margin-top: -15px;">
+                        $<?php
+                            $reward_points = isset($user_rewards['reward_points']) ? $user_rewards['reward_points'] : 0;
+                            $total_earning = isset($record['total_earning']) ? $record['total_earning'] : 0;
+                            echo number_format($reward_points + $total_earning, 2);
+                        ?>
+                    </h1>
+                    <!-- <p><a href="team.php" class="btn btn-info">Team Building</a></p> -->
+                    <p>Daily & Refer Total Earning</p>
                 </div>
             </div>
         </div>
+    </div>
+</div>
+
+
+
 
         <div class="col-12 mb-4">
             <div class="card">
