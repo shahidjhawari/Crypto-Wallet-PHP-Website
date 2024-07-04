71b5dac5e053f06739fc3fbf4a43cd1d8c345f92
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-07-04 09:57:16 +0500

    latest

--- header.php
+++ header.php
@@ -47,10 +47,10 @@
                         <a class="nav-link" href="staking.php">Stacking</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="team_design.php">Team Building</a>
+                        <a class="nav-link" href="logout.php">Daily Earning</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="logout.php">Daily Earning</a>
+                        <a class="nav-link" href="team_design.php">Team Building</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="bonus_rewards.php">Bonus Reward</a>
