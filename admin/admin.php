358bd2defa3f666a436508fe8761f66cdc3878f4
Shahid Iqbal <shahidiqbaljhawari>
2024-07-25 10:29:46 +0500

    latest

--- admin/admin.php
+++ admin/admin.php
@@ -1,7 +1,6 @@
 <?php
 // admin.php
 ob_start();
-session_start();
 require('top.inc.php');
 
 
@@ -14,9 +13,9 @@ if (isset($_SESSION['earnings_processed'])) {
 <div class="container">
     <h2>Admin Panel: Manage Daily Earnings</h2>
     <form method="post" action="calculate_daily_earnings.php">
-        <button type="submit" name="percentage" value="0.0035" class="btn btn-primary">Earn 0.45%</button>
-        <button type="submit" name="percentage" value="0.0055" class="btn btn-primary">Earn 0.55%</button>
-        <button type="submit" name="percentage" value="0.0065" class="btn btn-primary">Earn 0.65%</button>
+        <button type="submit" name="percentage" value="0.0035" class="btn btn-primary">Earn 0.35%</button>
+        <button type="submit" name="percentage" value="0.0045" class="btn btn-primary">Earn 0.45%</button>
+        <button type="submit" name="percentage" value="0.0055" class="btn btn-primary">Earn 0.65%</button>
     </form>
 </div>
 
