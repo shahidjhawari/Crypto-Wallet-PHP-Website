9339a8883571e1de5df7283ba8488dab293bce1f
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari>
2024-07-27 16:21:36 +0500

    latest

--- header.php
+++ header.php
@@ -83,6 +83,9 @@ $stmt->close();
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                        <a class="nav-link" href="profile.php">My Profile</a>
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="dashboard.php">Wallet</a>
                     </li>
