a2bd8a46f859e14645f10cad9f8c64019d807940
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari>
2024-07-30 15:09:03 +0500

    latets

--- admin/connection.inc.php
+++ admin/connection.inc.php
@@ -1,5 +1,4 @@
 <?php
-ob_start();
 session_start();
 $con=mysqli_connect("localhost","root","","coin");
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/coin/');
@@ -12,6 +11,6 @@ define('SITE_PATH','http://localhost/coin/');
 // define('SERVER_PATH', $_SERVER['DOCUMENT_ROOT'] . '/');
 // define('SITE_PATH', 'http://stackinghub.rf.gd/');
 
-define('PRODUCT_IMAGE_SERVER_PATH', SERVER_PATH . 'media/product/');
-define('PRODUCT_IMAGE_SITE_PATH', SITE_PATH . 'media/product/');
+// define('PRODUCT_IMAGE_SERVER_PATH', SERVER_PATH . 'media/product/');
+// define('PRODUCT_IMAGE_SITE_PATH', SITE_PATH . 'media/product/');
 ?>
