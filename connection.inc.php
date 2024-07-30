704821e649227dc1be7e9ee2e361b46e7527efa9
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari>
2024-07-30 15:12:08 +0500

    latest

--- connection.inc.php
+++ connection.inc.php
@@ -9,6 +9,6 @@ define('SITE_PATH', 'http://localhost/coin/');
 // define('SERVER_PATH', $_SERVER['DOCUMENT_ROOT'] . '/');
 // define('SITE_PATH', 'http://stackinghub.rf.gd/');
 
-// define('PRODUCT_IMAGE_SERVER_PATH', SERVER_PATH . 'media/product/');
-// define('PRODUCT_IMAGE_SITE_PATH', SITE_PATH . 'media/product/');
+define('PRODUCT_IMAGE_SERVER_PATH', SERVER_PATH . 'media/product/');
+define('PRODUCT_IMAGE_SITE_PATH', SITE_PATH . 'media/product/');
 ?>
