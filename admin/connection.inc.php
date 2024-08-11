c638f5d678041ac5f55db05278a435e7db303035
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari>
2024-08-11 08:34:54 +0500

    latest

--- admin/connection.inc.php
+++ admin/connection.inc.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-$con=mysqli_connect("localhost","root","","coin");
-define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/coin/');
-define('SITE_PATH','http://localhost/coin/');
+$con = mysqli_connect("localhost", "root", "", "coin");
+define('SERVER_PATH', $_SERVER['DOCUMENT_ROOT'] . '/coin/');
+define('SITE_PATH', 'http://localhost/coin/');
 
 
 // Online Code Here
@@ -11,6 +11,12 @@ define('SITE_PATH','http://localhost/coin/');
 // define('SERVER_PATH', $_SERVER['DOCUMENT_ROOT'] . '/');
 // define('SITE_PATH', 'http://stackinghub.rf.gd/');
 
+
+//Online Original Code Here
+
+// $conn = mysqli_connect("sql213.infinityfree.com", "if0_37082220", "bX5rPVvrqf0SDv", "if0_37082220_coin");
+// define('SERVER_PATH', $_SERVER['DOCUMENT_ROOT'] . '/');
+// define('SITE_PATH', 'http://stackinghub.rf.gd/');
+
 define('PRODUCT_IMAGE_SERVER_PATH', SERVER_PATH . 'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH', SITE_PATH . 'media/product/');
-?>
