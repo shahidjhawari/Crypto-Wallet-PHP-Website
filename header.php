d17439ae8401a9d306df523a7e0c209d01be7918
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari>
2024-08-16 09:11:50 +0500

    latest

--- header.php
+++ header.php
@@ -9,10 +9,6 @@ require('connection.inc.php');
 $user_id = $_SESSION['user_id'] ?? null;
 $user_name = $_SESSION['user_name'] ?? null;
 
-// Redirect to login page if user_id or user_name is not set
-if (!$user_id || !$user_name) {
-    // echo "hgf";
-}
 
 // Fetch user-specific data
 $stmt = $conn->prepare("SELECT * FROM rewards WHERE user_id = ?");
@@ -44,7 +40,6 @@ $stmt->execute();
 $wallet_balance_row = $stmt->get_result()->fetch_assoc();
 $wallet_balance = $wallet_balance_row['wallet_balance'] ?? 0;
 $stmt->close();
-
 ?>
 
 <!DOCTYPE html>
@@ -135,95 +130,97 @@ $stmt->close();
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary shadow-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="dashboard.php"><img src="img/logo2.png" width="40" alt=""></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fas fa-bars" style="color: white;"></i>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="profile.php">My Profile</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="dashboard.php">Wallet</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="transactionsDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
-                            Transactions
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="transactionsDropdown">
-                            <?php if ($transaction_status === 'accepted') : ?>
-                                <li><a class="dropdown-item" href="deposit.php" class="btn btn-info">Deposit</a></li>
-                            <?php elseif ($transaction_status === 'rejected') : ?>
-                                <li>
-                                    <p>Transaction Status: <?php echo htmlspecialchars($transaction_status); ?></p>
-                                </li>
-                                <li><a href="activate.php" class="dropdown-item">Resend Activation Request</a></li>
-                            <?php elseif ($transaction_status === 'pending') : ?>
-                                <li>
-                                    <p>Transaction Status: <?php echo htmlspecialchars($transaction_status); ?></p>
-                                </li>
-                            <?php elseif (!$transaction_status) : ?>
-                                <li>
-                                    <p><a href="activate.php" class="dropdown-item">Activate Account</a></p>
-                                </li>
-                            <?php endif; ?>
-                            <?php if ($transaction_status === 'accepted' && $deposit_status) : ?>
-                                <!-- <li><p>Deposit Status: <?php echo htmlspecialchars($deposit_status); ?></p></li> -->
-                            <?php endif; ?>
-                            <li><a class="dropdown-item" href="user_payment.php">Withdrawal</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="stackingDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
-                            Stacking
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="stackingDropdown">
-                            <li><a class="dropdown-item" href="staking.php">Stacking</a></li>
-                            <li><a class="dropdown-item" href="#">P2P (Coming Soon)</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="stacking_dummy.php">Daily Earning</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="teamDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
-                            Team Building
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="teamDropdown">
-                            <li><a class="dropdown-item" href="team_member.php">Team Building Member</a></li>
-                            <li><a class="dropdown-item" href="team_earning.php">Team Building Earning</a></li>
-                        </ul>
-                    </li>
-                    <!-- <li class="nav-item">
+    <?php if ($user_id && $user_name): ?>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary shadow-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="dashboard.php"><img src="img/logo2.png" width="40" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars" style="color: white;"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="profile.php">My Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="dashboard.php">Wallet</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="transactionsDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                                Transactions
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="transactionsDropdown">
+                                <?php if ($transaction_status === 'accepted') : ?>
+                                    <li><a class="dropdown-item" href="deposit.php" class="btn btn-info">Deposit</a></li>
+                                <?php elseif ($transaction_status === 'rejected') : ?>
+                                    <li>
+                                        <p>Transaction Status: <?php echo htmlspecialchars($transaction_status); ?></p>
+                                    </li>
+                                    <li><a href="activate.php" class="dropdown-item">Resend Activation Request</a></li>
+                                <?php elseif ($transaction_status === 'pending') : ?>
+                                    <li>
+                                        <p>Transaction Status: <?php echo htmlspecialchars($transaction_status); ?></p>
+                                    </li>
+                                <?php elseif (!$transaction_status) : ?>
+                                    <li>
+                                        <p><a href="activate.php" class="dropdown-item">Activate Account</a></p>
+                                    </li>
+                                <?php endif; ?>
+                                <?php if ($transaction_status === 'accepted' && $deposit_status) : ?>
+                                    <!-- <li><p>Deposit Status: <?php echo htmlspecialchars($deposit_status); ?></p></li> -->
+                                <?php endif; ?>
+                                <li><a class="dropdown-item" href="user_payment.php">Withdrawal</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="stackingDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                                Stacking
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="stackingDropdown">
+                                <li><a class="dropdown-item" href="staking.php">Stacking</a></li>
+                                <li><a class="dropdown-item" href="#">P2P (Coming Soon)</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="stacking_dummy.php">Daily Earning</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="teamDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                                Team Building
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="teamDropdown">
+                                <li><a class="dropdown-item" href="team_member.php">Team Building Member</a></li>
+                                <li><a class="dropdown-item" href="team_earning.php">Team Building Earning</a></li>
+                            </ul>
+                        </li>
+                        <!-- <li class="nav-item">
                         <a class="nav-link" href="bonus_rewards.php">Bonus Reward</a>
                     </li> -->
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact.php">Contact Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="logout.php">Logout</a>
-                    </li>
-                </ul>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Contact Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="logout.php">Logout</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
 
-    <div class="telegram-container">
-        <a href="#" class="telegram-icon" id="telegramIcon">
-            <i class="fab fa-telegram-plane"></i>
-        </a>
-        <div class="extra-icons" id="extraIcons">
-            <a href="https://t.me/Staking_HUB" class="telegram-icon" target="_blank">
+        <div class="telegram-container">
+            <a href="#" class="telegram-icon" id="telegramIcon">
                 <i class="fab fa-telegram-plane"></i>
             </a>
-            <a href="https://t.me/+CANG0BzUsWM2NmFk" class="profile-icon" target="_blank">
-                <i class="fas fa-user"></i>
-            </a>
+            <div class="extra-icons" id="extraIcons">
+                <a href="https://t.me/Staking_HUB" class="telegram-icon" target="_blank">
+                    <i class="fab fa-telegram-plane"></i>
+                </a>
+                <a href="https://t.me/+CANG0BzUsWM2NmFk" class="profile-icon" target="_blank">
+                    <i class="fas fa-user"></i>
+                </a>
+            </div>
         </div>
-    </div>
+    <?php endif; ?>
 
     <script>
         document.getElementById('telegramIcon').addEventListener('click', function() {
@@ -238,3 +235,6 @@ $stmt->close();
             }
         });
     </script>
+</body>
+
+</html>
